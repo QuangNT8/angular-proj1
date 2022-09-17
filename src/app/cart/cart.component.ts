855dc3cc25d19352e800cb6c8e6602cdb2b759d2
quyt2011 <nguyentrungquang102@gmail.com>
2022-09-17 12:36:42 +0700

    Create the checkout form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,9 +11,23 @@ export class CartComponent implements OnInit {
 
   items = this.cartService.getItems();
   
-  constructor(private cartService : CartService) { }
+  checkoutForm = this.fromBuilder.group({
+      name : '',
+      address : ''
+  });
 
+  constructor(
+      private cartService : CartService, 
+      private fromBuilder : FormBuilder
+  ) { }
+  
   ngOnInit(): void {
   }
 
+  onSubmit(): void{
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    window.alert('Your order has been submitted');
+    this.checkoutForm.reset();
+  }
 }
